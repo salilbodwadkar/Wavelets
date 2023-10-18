db88ab3fee671dcb9e4aa9a1b7eb720a9a3f9279
Salil Bodwadkar <134647164+salilbodwadkar@users.noreply.github.com>
2023-10-17 21:36:20 -0500

    Update compress.py

--- compress.py
+++ compress.py
@@ -1,8 +1,16 @@
 import numpy as np
 
-def compr(y, r):
-    abs_y = np.abs(y)
-    threshold = r * np.max(abs_y)
-    yc = np.where(abs_y < threshold, 0, y)
+def compress(w,r):
+    if (r < 0) or (r > 1):
+        raise Exception("R must be between zero and one")
+    N = len(w)
+    Nr = np.floor(N*r)
+    ww = np.sort(np.abs(w))
+    tol = np.abs(ww[Nr])
     
-    return yc
+    for i in range(N):
+        if np.abs(w[i]) < tol:
+             w[i] = 0  
+    wc = w
+    return wc
+    
